89f5c9d15d440599cfa66272758bf6c94eddf253
tcs-ms-poc <sreeraj.ar@outlook.com>
2017-10-10 08:13:04 -0400

    V1.0
    First version

---
+++ TheLightOfGita/Controllers/HomeController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheLightOfGita.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Dedication()
+        {
+            ViewBag.Message = "Dedication";
+
+            return View();
+        }
+
+        public ActionResult Preface()
+        {
+            ViewBag.Message = "Preface";
+
+            return View();
+        }
+        public ActionResult GitaChapterI()
+        {
+            ViewBag.Message = "Gita Chapter I";
+
+            return View();
+        }
+        public ActionResult GitaChapterII()
+        {
+            ViewBag.Message = "Gita Chapter II";
+
+            return View();
+        }
+        public ActionResult GitaChapterIII()
+        {
+            ViewBag.Message = "Gita Chapter III";
+
+            return View();
+        }
+        public ActionResult GitaChapterIV()
+        {
+            ViewBag.Message = "Gita Chapter IV";
+
+            return View();
+        }
+        public ActionResult GitaChapterV()
+        {
+            ViewBag.Message = "Gita Chapter V";
+
+            return View();
+        }
+        public ActionResult GitaChapterVI()
+        {
+            ViewBag.Message = "Gita Chapter VI";
+
+            return View();
+        }
+        public ActionResult GitaChapterVII()
+        {
+            ViewBag.Message = "Gita Chapter VII";
+
+            return View();
+        }
+        public ActionResult GitaChapterVIII()
+        {
+            ViewBag.Message = "Gita Chapter VIII";
+
+            return View();
+        }
+        public ActionResult GitaChapterIX()
+        {
+            ViewBag.Message = "Gita Chapter IX";
+
+            return View();
+        }
+        public ActionResult GitaChapterX()
+        {
+            ViewBag.Message = "Gita Chapter X";
+
+            return View();
+        }
+        public ActionResult GitaChapterXI()
+        {
+            ViewBag.Message = "Gita Chapter XI";
+
+            return View();
+        }
+        public ActionResult GitaChapterXII()
+        {
+            ViewBag.Message = "Gita Chapter XII";
+
+            return View();
+        }
+        public ActionResult GitaChapterXIII()
+        {
+            ViewBag.Message = "Gita Chapter XIII";
+
+            return View();
+        }
+        public ActionResult GitaChapterXIV()
+        {
+            ViewBag.Message = "Gita Chapter XIV";
+
+            return View();
+        }
+        public ActionResult GitaChapterXV()
+        {
+            ViewBag.Message = "Gita Chapter XV";
+
+            return View();
+        }
+        public ActionResult GitaChapterXVI()
+        {
+            ViewBag.Message = "Gita Chapter XVI";
+
+            return View();
+        }
+        public ActionResult GitaChapterXVII()
+        {
+            ViewBag.Message = "Gita Chapter XVII";
+
+            return View();
+        }
+        public ActionResult GitaChapterXVIII()
+        {
+            ViewBag.Message = "Gita Chapter XVIII";
+
+            return View();
+        }
+    }
+}
